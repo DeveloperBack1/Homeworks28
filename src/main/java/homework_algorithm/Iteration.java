6bdac5f060c0a515a374d46a6309e1d08487a6ba
Max50204 <schneidermaxim45@yahoo.com>
2024-09-09 21:52:51 +0200

    Homework_09_09_2024

---
+++ src/main/java/homework_algorithm/Iteration.java
@@ -0,0 +1,23 @@
+package homework_algorithm;
+
+public class Iteration {
+    public static void main(String[] args) {
+        int result = funIteration(5, 3);
+        System.out.println("Result: " + result);
+    }
+
+
+    private static int funIteration(int x, int y) {
+        // Печатаем начальные значения x и y
+        System.out.println("Initial: x = " + x + "; y = " + y);
+
+        while (x > 0) {
+            y = x + y; // Обновляем значение y, как это делалось в рекурсивной функции
+            x--; // Уменьшаем x на 1
+            System.out.println("x = " + x + "; y = " + y); // Печатаем текущие значения
+        }
+
+        return y; // Возвращаем результат
+    }
+
+}
